e490c49d571002d842ad5348c08cffd8e3b4bc37
SterFryNoodle <revangcruz@yahoo.com>
2024-04-17 22:17:51 -0700

    script now checks for neighbor nodes of current node, and adds to path; Colors the nodes if being explored or set as path

--- Assets/Pathfinding/PathFinder.cs
+++ Assets/Pathfinding/PathFinder.cs
@@ -5,10 +5,40 @@ using UnityEngine;
 public class PathFinder : MonoBehaviour
 {
     [SerializeField] Node currentSearchNode;
-    Vector2Int[] directions = {Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down};
+    Vector2Int[] directions = {Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down};    
+    GridManager gridManager;
+
+    Dictionary<Vector2Int, Node> grid;
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+        if (gridManager != null )
+        {
+            grid = gridManager.Grid;
+        }
+    }
 
     void Start()
     {
-        
+        ExploreNeighbors();
+    }
+
+    void ExploreNeighbors()
+    {
+        List<Node> neighbors = new List<Node>();
+
+        for (int i = 0; i < directions.Length; i++)
+        {
+            Vector2Int neighborCoords = currentSearchNode.coordinates + directions[i];
+            
+            if (grid.ContainsKey(neighborCoords))
+            {
+                neighbors.Add(grid[neighborCoords]);
+
+                grid[neighborCoords].isExplored = true; //Delete this line after testing.
+                grid[currentSearchNode.coordinates].isPath = true;
+            }
+        }
     }
 }
